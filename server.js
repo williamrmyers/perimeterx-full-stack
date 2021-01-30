d27035fc07cd71bd1676385c6b207ea55edfb289
williamrmyers <williamrmyers@example.com>
2021-01-29 16:54:06 -0800

    Added HTML Front-end

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 
 const express = require("express");
 const perimeterx = require("perimeterx-node-express");
+const path = require("path");
 
 const server = express();
+
+const PORT = 3000;
 
 /* px-module and cookie parser need to be initiated before any route usage */
 const pxConfig = {
@@ -15,9 +18,9 @@ perimeterx.init(pxConfig);
 
 /*  block users with high bot scores using px-module for the route /helloWorld */
 server.get("/", perimeterx.middleware, (req, res) => {
-  res.send("Hello from PX");
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-server.listen(3000, () => {
-  console.log("server started");
+server.listen(PORT, () => {
+  console.log(`server started on ${PORT}`);
 });
